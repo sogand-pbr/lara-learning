8ddff85a9fe2528f3d7410acd1de4d30e1a00cb3
Hooshmand <sogandprh@gmail.com>
2023-07-24 01:30:59 +0330

    ✨ can't find scss directory while using vite ? read scss.app's comments

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,6 @@ use Illuminate\Support\Facades\Route;
     Route::get('/contact', function () {
         return view('contact');
     });
+   Route::get('/welcome', function () {
+        return view('welcome');
+    });
