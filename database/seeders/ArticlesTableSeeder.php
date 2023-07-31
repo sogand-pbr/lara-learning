02cf7876b47a653158a1b978c591dc2ed7fe8196
Hooshmand <sogandprh@gmail.com>
2023-08-01 00:50:11 +0330

    create seeder

---
+++ database/seeders/ArticlesTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class  ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        foreach (range(1,10)as $item){
+            DB::table('articles')->insert([
+                'title'=>"article $item",
+                'slug'=>"article-$item",
+                'body'=>"this is artile $item",
+
+            ]);
+        }
+    }
+}
