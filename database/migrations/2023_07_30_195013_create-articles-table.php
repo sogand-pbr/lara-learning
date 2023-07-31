b28dad900169194422b1e3120e1a65b8204051de
Hooshmand <sogandprh@gmail.com>
2023-07-31 20:55:07 +0330

    create migration table ->debug

--- database/migrations/2023_07_30_195013_create-articles-table.php
+++ database/migrations/2023_07_30_195013_create-articles-table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->timestamp();
+            $table->string('title');
 
         });
     }
